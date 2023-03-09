f11470065672e2f1270a1e97e25f13753d2cf9d8
KABANYANA <joyeusekabanyana@gmail.com>
2023-03-09 08:57:33 +0300

    changes

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -9,7 +9,32 @@ println(words("SUZAN","CARL"))
     calculator.subtraction()
     calculator.division()
     calculator.Multiplication()
+    println( phrase(arrayOf("joy","mary","grace")))
 }
+
+class Calculator(var num1: Int, var num2: Int) {
+    fun addition() {
+        var sum = num1 + num2
+        println(" the addition of two numbers is:" + sum)
+    }
+
+    fun subtraction() {
+        var minus = num1 - num2
+        println(" the subtraction of two numbers is:" + minus)
+    }
+
+    fun division() {
+        var divide = num1 / num2
+        println(" the division of two numbers is:" + divide)
+    }
+
+    fun Multiplication() {
+        var product = num1 * num2
+        println(" the division of two numbers is:" + product)
+    }
+
+}
+
 fun words(word1:String,word2:String):String{
     if (word2.length>word1.length){
         return word2[0].toString()
@@ -21,47 +46,31 @@ fun words(word1:String,word2:String):String{
         return word1[0].toString()
     }
 }
-//Write and invoke one function that takes in an array of integers and returns
-//these 3 values: smallest, largest and average of all the elements
+
 
 fun numbers(number:Array<Int>):String{
    var smallest = number.min()
     var largest = number.max()
     var avg = number.average()
-//    return  smallest.toDouble()
-//    return  largest.toDouble()
-//    return avg.toDouble()
+
     var answer = println("the smallest is:$smallest the largest is:$largest and the average is $avg")
 
     return answer.toString()
 }
-//Write and call a function that takes in a string and splits it into all the
-//characters that constitute it. Your function should print out this output.
+
 fun sentences(sent:String){
   println(sent.split(' '))
 }
-//Write and call a function that takes in an array of strings, joins them all into
-//one string and returns it.
 
-//fun phrase(ph:Array<String>):String{
-//    return
-//}
 
-class Calculator(var num1:Int, var num2:Int){
-    fun addition(){
-        var sum = num1+num2
-        println(" the addition of two numbers is:"+sum)
-    }
-    fun subtraction(){
-        var minus = num1 - num2
-        println(" the subtraction of two numbers is:"+minus)
-    }
-    fun division(){
-        var divide = num1/num2
-        println(" the division of two numbers is:"+divide)
-    }
-    fun Multiplication(){
-        var product = num1*num2
-        println(" the division of two numbers is:"+product)
+fun phrase(ph:Array<String>):String{
+    for (i in ph) {
+        var b = i + i
+        return "the new string is:" + b
+
     }
 }
+
+
+
+
